da32b2f29e3ea86c142ba1abd4365709a54c931b
Manas Vegi <manasvegi@gmail.com>
2022-11-24 22:33:56 -0500

    optimal solution for paint houses

--- Leetcode/Python/paint_houses_2.py
+++ Leetcode/Python/paint_houses_2.py
@@ -15,3 +15,33 @@ class Solution:
                     best = min(best, costs[i][c1] + dp[i + 1][c2])
                 dp[i][c1] = best
         return min(dp[0])
+
+    #O(nk) solution with O(1) extra space
+    def minCostIIOptimal(self, costs: List[List[int]]) -> int:
+        N, K = len(costs), len(costs[0])
+        min1, min2 = float('inf'), float('inf')
+        idx1 = -1
+        for i, c in enumerate(costs[N - 1]):
+            if c < min1:
+                min2 = min1
+                min1 = c
+                idx1 = i
+            elif c < min2:
+                min2 = c
+        for i in range(N - 2, -1, -1):
+            newmin1, newmin2 = float('inf'), float('inf')
+            newidx1 = -1
+            for c1 in range(K):
+                curr = costs[i][c1]
+                if c1 == idx1:
+                    curr += min2
+                else:
+                    curr += min1
+                if curr < newmin1:
+                    newmin2 = newmin1
+                    newmin1 = curr
+                    newidx1 = c1
+                elif curr < newmin2:
+                    newmin2 = curr
+            min1, min2, idx1 = newmin1, newmin2, newidx1
+        return min1
